09e8b5795c468a2af9849a68bf4fae1a4b763caf
mallikam <mallikamo@umich.edu>
2023-07-17 18:18:37 +0530

    added adaptive colours for light/dark mode

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -12,56 +12,64 @@ struct ContentView: View
     @StateObject var gameState = GameState()
     var body: some View
     {
-        let borderSize = CGFloat(5)
-        Text(String(format: "X's : %d", gameState.crossScore))
-            .font(.custom("TrebuchetMS-Bold", size: 30))
-            // .fontWeight(.light)
-            .foregroundColor(Color.mint)
-            .padding(0)
-            // .rotationEffect(.degrees(180))
-        
-        VStack(spacing: borderSize)
+        ZStack {
+            Color(UIColor.systemGray6)
+                .ignoresSafeArea()
+        }
+        .overlay
         {
-            ForEach(0...2, id: \.self)
-            {
-                row in
-                HStack(spacing: borderSize)
+            VStack{
+                let borderSize = CGFloat(5)
+                Text(String(format: "X's : %d", gameState.crossScore))
+                    .font(.custom("TrebuchetMS-Bold", size: 30))
+                    //.fontWeight(.light)
+                    .foregroundColor(Color.mint)
+                    .padding(0)
+                    //.rotationEffect(.degrees(180))
+                
+                VStack(spacing: borderSize)
                 {
                     ForEach(0...2, id: \.self)
                     {
-                        column in
-                        let cell = gameState.board[row][column]
-                        Text(cell.displayTile())
-                            .font(.system(size: 90))
-                            .foregroundColor(cell.tileColor())
-                            .bold()
-                            .frame(maxWidth: .infinity, maxHeight: .infinity)
-                            .aspectRatio(1, contentMode: .fit)
-                            .background(Color.white)
-                            .onTapGesture {
-                                gameState.placeTile(row, column)
+                        row in
+                        HStack(spacing: borderSize)
+                        {
+                            ForEach(0...2, id: \.self)
+                            {
+                                column in
+                                let cell = gameState.board[row][column]
+                                Text(cell.displayTile())
+                                    .font(.system(size: 100))
+                                    .foregroundColor(cell.tileColor())
+                                    .bold()
+                                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                                    .aspectRatio(1, contentMode: .fit)
+                                    .background(Color(UIColor.systemGray6)) //light
+                                    .onTapGesture {
+                                        gameState.placeTile(row, column)
+                                    }
                             }
+                        }
                     }
                 }
+                .background(Color(UIColor.systemGray)) // dark
+                .padding()
+                .alert(isPresented: $gameState.showAlert)
+                {
+                    Alert(
+                        title: Text(gameState.alertText),
+                        dismissButton: .default(Text("Next game!"))
+                        {
+                            gameState.resetBoard()
+                        }
+                    )
+                }
+                Text(String(format: "O's : %d", gameState.dotScore))
+                    .font(.custom("TrebuchetMS-Bold", size: 30))
+                    .padding()
+                    .foregroundColor(Color.pink)
             }
         }
-        .background(Color.black)
-        .padding()
-        .alert(isPresented: $gameState.showAlert)
-        {
-            Alert(
-                title: Text(gameState.alertText),
-                dismissButton: .default(Text("Next game!"))
-                {
-                    gameState.resetBoard()
-                }
-            )
-        }
-        
-        Text(String(format: "O's : %d", gameState.dotScore))
-            .font(.custom("TrebuchetMS-Bold", size: 30))
-            .padding()
-            .foregroundColor(Color.pink)
     }
 }
 
